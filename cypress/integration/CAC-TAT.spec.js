038caf99d6521c62c950e6fb4b64a768694bb6c0
vinimussio1 <vinimussio123@gmail.com>
2023-03-15 10:21:14 -0300

    Testes Web Iniciais

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,61 @@
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    beforeEach(function() {
+        cy.visit('./src/index.html')
+    })
+    
+    it('verifica o título da aplicação', function() {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+    
+    //inserindo valores
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Vinicius')
+        cy.get('#lastName').type('Aires')
+        cy.get('#email').type('vinicius@gmail.com')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
+    })
+
+    //selecionando valores
+    it('seleciona um produto (YouTube) por seu texto', function() {
+        cy.get('#product')
+            .select('YouTube')
+            .should('have.value', 'youtube')
+    })
+
+    //Radio Button
+    it('marca o tipo de atendimento "Feedback"', function() {
+        cy.get('input[type="radio"][value="feedback"]')
+            .check()
+            .should('have.value', 'feedback')
+    })
+
+    //Checkbox
+    it('marca ambos checkboxes, depois desmarca o último', function() {
+        cy.get('input[type="checkbox"]')
+            .check()
+            .should('be.checked')
+            .last()
+            .uncheck()
+            .should('not.be.checked')
+    })
+
+    //Select files
+    it.only('seleciona um arquivo da pasta fixtures', function() {
+        cy.get('input[type="file"]#file-upload')
+           .should('not.have.value')
+            .selectFile('./cypress/fixtures/example.json')
+            .should(function($input) {
+                expect(input[0].files[0].name).to.equal('example.json')
+            })
+
+    })
+
+
+
+})
+  
