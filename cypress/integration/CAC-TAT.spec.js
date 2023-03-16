232a86f81ad2209e3cfd5c06710bbcf33c9fc10e
vinimussio1 <vinimussio123@gmail.com>
2023-03-16 11:46:51 -0300

    CI Pipeline

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -59,7 +59,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
        cy.get('#privacy a').should('have.attr', 'target', '_blank')
     })
 
-    it.only('acessa a página da política de privacidade removendo o target e então clicando no link', function() {
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', function() {
         cy.get('#privacy a')
             .invoke('removeAttr', 'target')
             .click()
